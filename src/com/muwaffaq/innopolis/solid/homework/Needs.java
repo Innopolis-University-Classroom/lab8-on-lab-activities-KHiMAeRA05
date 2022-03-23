5b42aa4671826aed803d2cd1c4090c09e767e202
Aleksandra <45710112+KHiMAeRA05@users.noreply.github.com>
2022-03-23 19:50:36 +0300

    Update Needs.java

--- src/com/muwaffaq/innopolis/solid/homework/Needs.java
+++ src/com/muwaffaq/innopolis/solid/homework/Needs.java
@@ -1,18 +1,23 @@
 package com.muwaffaq.innopolis.solid.homework;
 
-public interface Needs {
+public interface Needs {}
 
+public interface doPray{
     void pray();
+}
 
+public interface doPlaySports{
     void playSports();
+}
 
+public interface doGetMarried{
     void getMarried();
+}
 
+public interface doOwnCompany{
     void ownCompany();
+}
 
+public interface doBecomeEmployee{
     void becomeEmployee();
-
-
-
-
 }
