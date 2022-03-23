75c168ec95349d616ce3a2e0d27b5832054cfdf7
Aleksandra <45710112+KHiMAeRA05@users.noreply.github.com>
2022-03-23 21:12:31 +0300

    Update Calculator.java

--- src/com/muwaffaq/innopolis/solid/OCP_DI/correct/Calculator.java
+++ src/com/muwaffaq/innopolis/solid/OCP_DI/correct/Calculator.java
@@ -1,76 +1,75 @@
-package com.muwaffaq.innopolis.solid.OCP_DI.wrong;
+package com.muwaffaq.innopolis.solid.OCP_DI.correct;
 
- interface Operation {
-   double calculate();
- }
- class Add implements Operation{
-   private double left;
-   private double right;
-   
-   Add(double left, double right){
+interface Operation {
+    double calculate();
+}
+class Add implements Operation{
+    private final double left;
+    private final double right;
+    
+    Add(double left, double right){
       this.left = left;
       this.right = right;
-   }
-  
-   @Override
-   public double calculate(double left, double right){
+}
+
+    @Override
+    public double calculate(){
       return left + right;
-   };
- }
- class Substract implements Operation{
-   private double left;
-   private double right;
-  
-   Substract(double left, double right){
-      this.left = left;
-      this.right = right;
-   }
-  
-   @Override
-   public double calculate(double left, double right){
-      return left - right;
-   };
- }
- class Divide implements Operation{
-   private double left;
-   private double right;
-  
-   Divide(double left, double right){
-      this.left = left;
-      this.right = right;
-   }
-  
-   @Override
-   public double calculate(double left, double right){
-      return left / right;
-   };
- }
- class Multiplication implements Operation{
-   private double left;
-   private double right;
-  
-   Multiplication(double left, double right){
-      this.left = left;
-      this.right = right;
-   }
-  
-   @Override
-   public double calculate(double left, double right){
-      return left * right;
-   };
- }
- class CalculatorCorrect {
-    public  double calculate(Operation operation) {
-        return operation.calculate(left, right);
-    }
-
-    public static void main(String[] args) {
-        CalculatorCorrect calculator = new CalculatorCorrect();
-        calculator.calculate(new Add());
-        calculator.calculate(new Add());
-
     }
 
 }
+class Substract implements Operation{
+    private final double left;
+    private final double right;
+    
+    Substract(double left, double right){
+        this.left = left;
+        this.right = right;
+    }
 
+    @Override
+    public double calculate(){
+        return left - right;
+    }
+}
+class Divide implements Operation{
+    private final double left;
+    private final double right;
+    
+    Divide(double left, double right){
+        this.left = left;
+        this.right = right;
+    }
+    
+    @Override
+    public double calculate(){
+        return left - right;
+    }
+}
+class Multiplication implements Operation{
+    private final double left;
+    private final double right;
+    
+    Multiplication(double left, double right){
+        this.left = left;
+        this.right = right;
+    }
+    
+    @Override
+    public double calculate(){
+        return left - right;
+    }
+}
+class CalculatorCorrect {
+    public double calculate(Operation operation) {
+        return operation.calculate();
+    }
+    
+    public static void main(String[] args) {
+        CalculatorCorrect calculator = new CalculatorCorrect();
+        calculator.calculate(new Add(5, 6));
+        calculator.calculate(new Add(7, 8));
+    
+    }
 
+}
